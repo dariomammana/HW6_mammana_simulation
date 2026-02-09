963bfce4f9f3599e0484000d806fb7a32a9f19cd
Jascha Grübel <jgruebel@Jaschas-MacBook-Pro.local>
2026-02-09 21:44:08 +0100

    add coloar dynamic console

---
+++ main.py
@@ -0,0 +1,142 @@
+
+# Our class OOP project here
+
+import string 
+import random
+import sys    
+import time   
+import os
+
+if os.name == 'nt':
+    os.system('color')
+
+class Lifeform:
+    
+    def __init__(self, location = None):
+        self.location = location
+        self._r = random.randint(0,255)
+        self._g = random.randint(0,255)
+        self._b = random.randint(0,255)
+        self.symbol = f"\x1b[38;2;{self._r};{self._g};{self._b}m{random.choice(string.ascii_letters)}\x1b[0m"
+        
+    def act(self, map):
+        self.move(map)
+    
+    def move(self, map):
+        x_offset =random.randint(-1, 1)
+        y_offset =random.randint(-1, 1)
+        new_x = self.location.x+x_offset
+        new_y = self.location.y+y_offset
+        if 0<new_x and new_x < len(map.cells) and 0<new_y and new_y < len(map.cells):
+            if not map.cells[new_x][new_y].inhabitant:
+                self.location.inhabitant = None
+                self.location = map.cells[new_x][new_y]
+                map.cells[new_x][new_y].inhabitant = self
+            
+    
+    def render(self):
+        return(self.symbol)
+
+class Map:
+    
+    def __init__(self, n = 10):
+        self.size = n
+        self.cells = dict()
+        for i in range(0,self.size):
+            self.cells[i] = dict()
+            for j in range(0,self.size):
+                self.cells[i][j] = Cell(i,j)
+                
+    def render(self):
+        render_str = ""
+        for i in range(0,self.size):
+            render_str += "\n"
+            for j in range(0,self.size):
+                render_str += self.cells[i][j].render() + " "
+        return(render_str)
+    
+class Cell:
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.state = "."
+        self.inhabitant = None
+        
+    def render(self):
+        if (self.inhabitant):
+            return self.inhabitant.render()
+        else:
+            return self.state
+
+class Simulation:
+    
+    def __init__(self, timestep = 0.125, iterations = 3, map_size = 10,lifeform_count = 5):
+        self._dynamic_terminal = DynamicTerminal(map_size+2)
+        self.max_iter = iterations
+        self.time_step = timestep
+        self.current_iter = 0
+        self.terminated = False
+        self.lifeform_count = lifeform_count
+        self.map_size = map_size
+        self.map = Map(map_size)
+        self.lifeforms = list()
+        self.setup()
+        
+    def setup(self):
+        for i in range(0,self.lifeform_count):
+            x = random.randint(0, self.map_size-1) 
+            y = random.randint(0, self.map_size-1)
+            new_life = Lifeform(self.map.cells[x][y])
+            self.lifeforms.append(new_life)
+            self.map.cells[x][y].inhabitant = new_life
+        
+        
+    def step(self):
+        if self.current_iter < self.max_iter:
+            self.current_iter += 1
+            self.update()
+            self.render()
+        else:
+            self.terminated = True
+            print(f"The simulation has terminated after {self.current_iter} iterations.")
+            
+    def update(self):
+        time.sleep(self.time_step)
+        for lifeform in self.lifeforms:
+            lifeform.act(self.map)
+    
+    def render(self):
+        self._dynamic_terminal.render(["Round " + str(self.current_iter),self.map.render()],self.map_size+2)
+        
+class DynamicTerminal:
+    """The dynamic termin provides an interface to repaint multiple rows in the terminal and animate the simulation"""
+
+    def __init__(self,nrows = 12):
+        for i in range(0,nrows):
+            print("")
+
+    def move_cursor_up(self, lines: int):
+        """Move cursor up <lines> lines."""
+        sys.stdout.write(f'\x1b[{lines}A')   # ESC[<n>A
+        sys.stdout.flush()
+
+    def rewrite_lines(self, new_lines):
+        """Write <new_lines> starting at the current cursor position."""
+        sys.stdout.write('\n'.join(new_lines) + '\n')
+        sys.stdout.flush()
+
+    def clear_line(self):
+        """Clear the entire current line (ESC[2K)."""
+        sys.stdout.write('\x1b[2K')
+        sys.stdout.flush()
+
+    def render(self, text, nrows):
+        self.move_cursor_up(nrows)
+        self.clear_line()
+        self.rewrite_lines(text)
+        
+if __name__ == "__main__":
+    sim = Simulation(iterations=50,map_size = 30,lifeform_count = 50)
+    while(not sim.terminated):
+        sim.step()
